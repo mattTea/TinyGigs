c06af47888b924121d95484be2a78f2c34b57d5b
Matt Thompson <vi2632@VI-LAC02VM341G8WN.broadband>
2018-07-02 19:21:07 +0100

    Styles nav buttons to include active styles

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavHeader = () => (
+const NavHeader = (props) => (
   <header className="container vertical-align">
-    <NavLink to="/" exact={true}>
-    <div className="gigtype-button">  
-      <img src={require('../../public/images/music.png')} style={{maxWidth: "1.5em", maxHeight: "1.5em", padding: "0 .5em"}}></img>
-    </div>
+    {console.log()}
+    <NavLink
+      className="gigtype-button"
+      to="/"
+      exact={true}
+      activeStyle={{
+        // backgroundColor: 'rgb(85, 1, 1)',
+        backgroundColor: '#1779ba',
+        cursor: 'default',
+        transform: 'none',
+        boxShadow: '1.5px 1.5px .75px black'
+      }}>
+      <div className="nav-image">  
+        <img src={require('../../public/images/music.png')} style={{maxWidth: "1.5em", maxHeight: "1.5em", padding: "0 .5em"}}></img>
+      </div>
     </NavLink>
     <NavLink to="/" exact={true}>
       <img src={require('../../public/images/CaravanGig2.png')} style={{maxWidth: "10em", maxHeight: "10em"}}></img>
     </NavLink>
-    <NavLink to="/plays">
-      <div className="gigtype-button">
+    <NavLink
+      className="gigtype-button"
+      to="/plays"
+      exact={true}
+      activeStyle={{
+        backgroundColor: '#1779ba',
+        cursor: 'default',
+        transform: 'none',
+        boxShadow: '1.5px 1.5px .75px black'
+      }}>
+      <div className="nav-image">
         <img src={require('../../public/images/theatre.png')} style={{maxWidth: "3em", maxHeight: "3em"}}></img>
       </div>
     </NavLink>
